78f0b50898d4081a819dc7afe14ddf0352f87537
Nick <Nick@128.4.136.197>
2017-03-18 16:47:07 -0400

    Gave it my best

--- XMLExample/src/main/java/pkgLibrary/Book.java
+++ XMLExample/src/main/java/pkgLibrary/Book.java
@@ -15,12 +15,13 @@ public class Book {
 	private double price;
 	private Date publish_date;
 	private String description;
+	private double cost;
 
 	public Book() {
 
 	}
 
-	public Book(String id, String author, String title, String genre, double price, Date publish_date, String description)
+	public Book(String id, String author, String title, String genre, double price, Date publish_date, String description, double cost)
 	{
 		super();
 		this.id = id;
@@ -30,10 +31,9 @@ public class Book {
 		this.price = price;
 		this.publish_date = publish_date;
 		this.description = description;
+		this.cost = cost;
 	}
 	
- 
-
 	public String getId() {
 		return id;
 	}
@@ -97,7 +97,12 @@ public class Book {
 		this.description = description;
 	}
 
-	
-	
+	public double getCost() {
+		return cost;
+	}
 
+	@XmlElement
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
 }
--- XMLExample/src/main/java/pkgLibrary/Catalog.java
+++ XMLExample/src/main/java/pkgLibrary/Catalog.java
@@ -1,7 +1,12 @@
 package pkgLibrary;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.*;
 
 @XmlRootElement
@@ -15,12 +20,10 @@ public class Catalog {
 	@XmlElement(name="book")
 	ArrayList<Book> books;
 	
-	
 	public int getId() {
 		return id;
 	}
 	
-
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -28,18 +31,87 @@ public class Catalog {
 		return books;
 	}
 	
-
 	public void setBooks(ArrayList<Book> books) {
 		this.books = books;
 	}
 	
+	public static Book GetBook(Catalog cat,String BookID) throws BookException{
+		Book FoundBook = null;
+		
+		for(Book b : cat.getBooks()){
+			if(b.getId().equals(BookID)){
+				FoundBook = b;
+			}
+		}
+		if(FoundBook instanceof Book){
+			return FoundBook;
+		}
+		else{
+			throw new BookException(BookID);
+		}
+	}
+	
+	public static void AddBook(int CatalogID, Book newBook) throws BookException{
+		
+		boolean BookExists = false;
+		
+		Catalog cat = null;
 
-	
-	
+		String basePath = new File("").getAbsolutePath();
+		basePath = basePath + "\\src\\main\\resources\\XMLFiles\\Books.xml";
+		File file = new File(basePath);
 
+		System.out.println(file.getAbsolutePath());
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			cat = ((Catalog) jaxbUnmarshaller.unmarshal(file));
+			System.out.println(cat);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		
+		if(cat.getId()==CatalogID){
+			
+			for(Book b : cat.getBooks()){
+				
+				if(b.getId().equals(newBook.getId())){
+					
+					BookExists = true;
+				}
+			}
+			if(BookExists){
+				
+				throw new BookException(newBook);
+			}
+			else{
+				cat.getBooks().add(newBook);
+				
+				try {
 
-	
-	
-	
-	
+					basePath = new File("").getAbsolutePath();
+					basePath = basePath + "\\src\\main\\resources\\XMLFiles\\Books.xml";
+					file = new File(basePath);
+
+					JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
+					Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+					// output pretty printed
+					jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+					jaxbMarshaller.marshal(cat, file);
+					jaxbMarshaller.marshal(cat, System.out);
+
+				} catch (JAXBException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		else{
+			
+			System.out.println("Catalog ID not found.");
+		}
+
+	}
+
 }
